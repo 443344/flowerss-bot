2e68b9cf4f3e70b710c9de1947ad1ab0eaa737ee
443344 <63106540+443344@users.noreply.github.com>
2020-04-17 21:05:40 +0800

    Update publish.go

--- tgraph/publish.go
+++ tgraph/publish.go
@@ -18,10 +18,7 @@ func PublishHtml(sourceTitle string, title string, rawLink string, html string) 
 	//)
 
 	html = html + fmt.Sprintf(
-		"<hr><p>本文章由 <a href=\"https://www.443344.xyz\">Alure资源社</a> 发布。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
-		rawLink,
-		title,
-		sourceTitle,
+		"<p><img src="https://img.rruu.net/image/5e99a55a3e074"/></p><hr><p>本文章由 <a href=\"https://www.443344.xyz\">Alure资源社</a> 收集。</p><p>查看tg频道 <a href=\"https://t.me/best00000">@best00000</a> 获取更多信息。</p>"
 	)
 	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
 	client := clientPool[rand.Intn(len(clientPool))]
