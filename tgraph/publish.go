bbd9cfede9c75b5be3f9956f08a849dea8564d9d
443344 <63106540+443344@users.noreply.github.com>
2020-04-18 14:20:20 +0800

    Update publish.go

--- tgraph/publish.go
+++ tgraph/publish.go
@@ -5,20 +5,22 @@ import (
 	"log"
 	"math/rand"
 	"time"
+	"io/ioutil"
 )
 
 func PublishHtml(sourceTitle string, title string, rawLink string, html string) (string, error) {
-	//html = fmt.Sprintf(
-	//	"<p>本文章由 <a href=\"https://www.443344.xyz\">Alure资源社</a> 发布。</p><hr>",
-	//) + html + fmt.Sprintf(
-	//	"<hr><p>本文章由 <a href=\"https://www.443344.xyz\">Alure资源社</a> 发布。</p><p>查看原文：<a href=\"%s\">%s - %s</p>",
-	//	rawLink,
-	//	title,
-	//	sourceTitle,
-	//)
+
+     headdata, err := ioutil.ReadFile("/root/flowerss-bot/tgraph/abc.txt")
+	if err != nil {
+		fmt.Println("File reading error", err)
+		headd := string(headdata)
+		return headd,err
+	}
+	headd := string(headdata)
 
 	html = html + fmt.Sprintf(
-		"<p><img src="https://img.rruu.net/image/5e99a55a3e074"/></p><hr><p>本文章由 <a href=\"https://www.443344.xyz\">Alure资源社</a> 收集。</p><p>查看tg频道 <a href=\"https://t.me/best00000">@best00000</a> 获取更多信息。</p>"
+		"<hr>%s<hr><p>本文章由 <a href=\"https://www.443344.xyz\">Alure资源社</a> 收集整理。</p><p>加入tg频道：<a href=\"https://t.me/best00000\">@best00000</p>",
+		headd,
 	)
 	rand.Seed(time.Now().Unix()) // initialize global pseudo random generator
 	client := clientPool[rand.Intn(len(clientPool))]
